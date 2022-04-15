748ad9d0c8f85edf8735c10c6f8532a328b161ad
Muhammad Bilal <85237387+ArbitechSolution@users.noreply.github.com>
2022-04-15 15:20:19 +0500

    Dropdown final

--- src/Component/SideBar/MediaSidebar.js
+++ src/Component/SideBar/MediaSidebar.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React, { useEffect, useState, useLayoutEffect } from 'react'
 import Growth from "../../Assets/Growth.png"
 import Frame2 from "../../Assets/Frame 2.png"
 import Frame3 from "../../Assets/Frame 3.png"
@@ -13,10 +13,19 @@ import { Link, NavLink, useNavigate, useHistory } from "react-router-dom";
 import VectorDown from "../../Assets/VectorDown.png"
 import "./SideBar.css"
 import { IoIosArrowDown } from 'react-icons/io';
+import Group480 from "../../Assets/Group 480.png"
+import $ from 'jquery';
 function MediaSidebar() {
   const navigate = useNavigate();
-
+  let [dropdown, setDropDown] = useState(false)
+  let [updateDropDown, setUpdateDropDown] = useState({
+    name: "Staking & Point",
+    img: Group480
+  })
   let newarry = [{
+    name: "ROAD POOL",
+    img: Group480
+  }, {
     name: "Staking & Point",
     img: Growth
   },
@@ -30,11 +39,11 @@ function MediaSidebar() {
   },
   {
     name: "Mystery Box",
-    img: Frame3
+    img: Box
   },
   {
     name: "NFT Staking",
-    img: Frame3
+    img: nft1
   }, {
     name: "NFT Market",
     img: marketplace1
@@ -44,34 +53,124 @@ function MediaSidebar() {
     img: Group593
   }
   ]
+
   const changeAutoNavgate = () => {
     let path = window.location.pathname
     path = path.split('/')
     console.log("path", path[1]);
-    if(path[1]==''){
-      let element =   document.getElementById("newlocale");
-      element.value = "/";
-    }else{
-       let element = document.getElementById("newlocale");
-       element.value =  `/${path[1]}`;
+    if (path[1] == '') {
+      setUpdateDropDown({
+        name: "ROAD POOL",
+        img: Group480
+      })
+    } else if (path[1] == 'Staking') {
+      setUpdateDropDown({
+        name: "Staking & Point",
+        img: Growth
+      })
+    } else if (path[1] == "mint") {
+      setUpdateDropDown({
+        name: "Mint",
+        img: Frame2
+      })
+    } else if (path[1] == "breed") {
+      setUpdateDropDown({
+        name: "Breed",
+        img: Frame3
+      })
+    } else if (path[1] == "MysteryBox") {
+      setUpdateDropDown({
+        name: "Mystery Box",
+        img: Box
+      })
+    } else if (path[1] == "NFTstaking") {
+      setUpdateDropDown({
+        name: "NFT Staking",
+        img: nft1
+      })
+    } else if (path[1] == "marketplace") {
+      setUpdateDropDown({
+        name: "NFT Market",
+        img: marketplace1
+      })
+    } else if (path[1] == "MyNFTs") {
+      setUpdateDropDown({
+        name: "My NFT",
+        img: Group593
+      })
     }
   }
-  useEffect(()=>{
+  useLayoutEffect(() => {
     changeAutoNavgate()
-  },[])
-  const changeNavigate = (event) => {
-    navigate(event.target.value)
+  }, [])
+  const changeNavigate = (image, name) => {
+    setUpdateDropDown({
+      name: name,
+      img: image
+    })
+    if (name == "Staking & Point") {
+      navigate('/Staking')
+    } else if (name == "Mint") {
+      navigate('/mint')
+    } else if (name == "Breed") {
+      navigate('/breed')
+    } else if (name == "Mystery Box") {
+      navigate('/MysteryBox')
+    } else if (name == "NFT Staking") {
+      navigate('/NFTstaking')
+    } else if (name == "NFT Market") {
+      navigate('/marketplace')
+    } else if (name == "My NFT") {
+      navigate('/MyNFTs')
+    } else if (name == "ROAD POOL") {
+      navigate('/')
+    }
   }
+
   return (
-    <div className=' pt-5' style={{backgroundColor: "#111217"}}>
+    <div className=' pt-5' style={{ backgroundColor: "#111217" }}>
 
-   <div className='row d-flex justify-content-center'>
-      <div className='col-11 staking-coll'  style={{}}>
+      <div className='row d-flex justify-content-center'>
+        <div className='col-11 staking-coll' style={{}}>
 
+          {/* <img src={VectorDown} className="VectorDown-image" /> */}
+          {/* <select className="vodiapicker" id="newlocale" onChange={(event) => changeNavigate(event)}>
+            <option value="/" className="test" data-thumbnail=""
 
+            >ROAD POOL</option>
+            <option value="/Staking" data-thumbnail="https://i.ibb.co/9pYHPw7/Growth.png"
 
+            >Staking & Point</option>
+            <option value="/Mint" data-thumbnail="https://i.ibb.co/x35Vvzx/Frame-2.png">Mint</option>
+            <option value="/breed" data-thumbnail="https://i.ibb.co/KjRcJGP/Frame-3.png">Breed</option>
+            <option value="/MysteryBox" data-thumbnail="https://i.ibb.co/W6RCYTf/Box.png">Mystery Box</option>
+            <option value="/NFTstaking" data-thumbnail="https://i.ibb.co/47Cdy4c/nft-1.png">NFT Staking</option>
+            <option value="/marketplace" data-thumbnail="https://i.ibb.co/W0LyT7X/marketplace-1.png">NFT Market</option>
+            <option value="/MyNFTs" data-thumbnail="https://i.ibb.co/QcKJYxG/Group-593.png">My NFT</option>
+          </select> */}
+          <div className="lang-select" id="newlocale"
+            onClick={() => setDropDown(!dropdown)}
+          >
+            <img src={VectorDown} className="VectorDown-image" />
+            <button className="btn-select"
 
-      <div className='form-group ' >
+            ><ul><li><img src={updateDropDown.img} /> <span className='ms-3 mt-4'>{updateDropDown.name}</span></li></ul> </button>
+            <div className={dropdown ? "b" : "b1"}
+
+            >
+              <ul id="a">
+                {newarry.map((item, index) => {
+                  return <>
+                    <li
+                      onClick={() => changeNavigate(item.img, item.name)}
+                    ><img src={item.img} /> {item.name}</li>
+                  </>
+                })}
+
+              </ul>
+            </div>
+          </div>
+          {/* <div className='form-group ' >
 
         <img src={VectorDown} className="VectorDown-image"/>
         <select id="newlocale" className='form-select selector' aria-label="Default select example"
@@ -91,10 +190,10 @@ style={{width: "200px"}}
           <option value="/MyNFTs">My NFT</option>
         </select>
 
-      </div>
+      </div> */}
 
 
-      {/* <div class="accordion " id="accordionFlushExample">
+          {/* <div class="accordion " id="accordionFlushExample">
                 <div class="accordion-item staking-boxResponsive">
                     <h2 class="accordion-header" id="headingTwo">
                         <button class="accordion-button collapsed" value="0" type="button" style={{ color: "white" }} data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
@@ -169,8 +268,8 @@ style={{width: "200px"}}
                     </div>
                 </div>
             </div> */}
-             </div>
-             </div>
+        </div>
+      </div>
     </div>
   )
 }
