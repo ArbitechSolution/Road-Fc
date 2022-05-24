31bb8b732d02c31eaf5511714e0d0ebf1da584a2
ArbitechSolution <arbitechsolutions@gmail.com>
2022-05-24 06:00:27 -0700

    formated reducers

--- src/Component/Redux/combineReducer.js
+++ src/Component/Redux/combineReducer.js
@@ -1,37 +1,35 @@
-import {combineReducers} from 'redux';
-import {connectWallet,} from './reducers/getWalletAddrss'
-import {getThbbalance} from './reducers/getUserThbBalance'
-import{getThbLpbalance} from './reducers/getUserThbLpBalance'
-import {tAmount} from './reducers/getUserthbTamount'
-import {tAmountLp} from './reducers/getUserthbLpTamount'
-import {getUserBrlpoint} from './reducers/getUserBrlPoint'
-import{getUserBrLplpoint} from './reducers/getUserBrlLpPoint'
-import {getBrawlPointMint} from './reducers/getUsersbrwalPointMint'
-import {setCurrentBpTokens} from "./reducers/getCurrentBpTokens";
-import { setMaxBpTokens } from './reducers/getMaxBpTokens';
-import {setPreSaleInfo} from './reducers/setPreSaleInfo';
-import {setUserBalance} from './reducers/setUserBalance';
-import {setUserDepositTime} from './reducers/setUserDepositTime';
-import {setMintingInfo} from './reducers/setMintingInfo';
-import {setUserReward} from './reducers/setUserReward';
+import { combineReducers } from "redux";
+import { connectWallet } from "./reducers/getWalletAddrss";
+import { getThbbalance } from "./reducers/getUserThbBalance";
+import { getThbLpbalance } from "./reducers/getUserThbLpBalance";
+import { tAmount } from "./reducers/getUserthbTamount";
+import { tAmountLp } from "./reducers/getUserthbLpTamount";
+import { getUserBrlpoint } from "./reducers/getUserBrlPoint";
+import { getUserBrLplpoint } from "./reducers/getUserBrlLpPoint";
+import { getBrawlPointMint } from "./reducers/getUsersbrwalPointMint";
+import { setCurrentBpTokens } from "./reducers/getCurrentBpTokens";
+import { setMaxBpTokens } from "./reducers/getMaxBpTokens";
+import { setPreSaleInfo } from "./reducers/setPreSaleInfo";
+import { setUserBalance } from "./reducers/setUserBalance";
+import { setUserDepositTime } from "./reducers/setUserDepositTime";
+import { setMintingInfo } from "./reducers/setMintingInfo";
+import { setUserReward } from "./reducers/setUserReward";
 const allReducer = combineReducers({
-
-    connectWallet:connectWallet,
-    getThbbalance:getThbbalance,
-    getThbLpbalance:getThbLpbalance,
-    tAmount:tAmount,
-    tAmountLp:tAmountLp,
-    getUserBrlpoint:getUserBrlpoint,
-    getUserBrLplpoint:getUserBrLplpoint,
-    getBrawlPointMint:getBrawlPointMint,
-    setMaxBpTokens:setMaxBpTokens,
-    setCurrentBpTokens:setCurrentBpTokens,
-    preSaleInfo : setPreSaleInfo,
-    userBalance:setUserBalance,
-    userDepositTime: setUserDepositTime,
-    mintngInfo:setMintingInfo,
-    userReward:setUserReward,
-
+  connectWallet: connectWallet,
+  getThbbalance: getThbbalance,
+  getThbLpbalance: getThbLpbalance,
+  tAmount: tAmount,
+  tAmountLp: tAmountLp,
+  getUserBrlpoint: getUserBrlpoint,
+  getUserBrLplpoint: getUserBrLplpoint,
+  getBrawlPointMint: getBrawlPointMint,
+  setMaxBpTokens: setMaxBpTokens,
+  setCurrentBpTokens: setCurrentBpTokens,
+  preSaleInfo: setPreSaleInfo,
+  userBalance: setUserBalance,
+  userDepositTime: setUserDepositTime,
+  mintngInfo: setMintingInfo,
+  userReward: setUserReward,
 });
 
 export default allReducer;
